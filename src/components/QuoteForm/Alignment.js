a443dc41c220c63253afec1723d3255fad436577
Max Antonucci <maxx1128@gmail.com>
2021-08-12 12:45:32 -0400

    Add alignment slider inputs

--- src/components/QuoteForm/Alignment.js
+++ src/components/QuoteForm/Alignment.js
@@ -8,16 +8,20 @@ const Alignment = ({  setPosX, updatePosX,  setPosY, updatePosY }) => (
     <p>
         Both inputs represent the <strong>percentage position</strong> for horizontal and vertical alignment. Based on the image's size, one of these alignments won't change the image.
     </p>
-    <div className="qig-l-wrapper__form-multi">
-        <div>
-          <p className="qig-p--short-label">Horizontal</p>
-          <input type="number" min="0" max="100" name="posX" value={setPosX} onChange={updatePosX} />
-        </div>
+    <div className="qig-l-slider__container">
+        <label className="qig-l-slider__label">
+          <span>
+            Horizontal: <strong>{setPosX}%</strong>
+          </span>
+          <input type="range" min="0" max="100" name="posX" value={setPosX} onChange={updatePosX} />
+        </label>
 
-        <div>
-          <p className="qig-p--short-label">Vertical</p>
-          <input type="number" min="0" max="100" name="posY" value={setPosY} onChange={updatePosY} />
-        </div>
+        <label className="qig-l-slider__label">
+          <span>
+            Vertical: <strong>{setPosY}%</strong>
+          </span>
+          <input type="range" min="0" max="100" name="posY" value={setPosY} onChange={updatePosY} />
+        </label>
     </div>
   </div>
 );
